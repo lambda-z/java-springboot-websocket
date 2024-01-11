0856c3a3f8229beb024ed17b5021a73345a4271b
chuan <18048587325@163.com>
2024-01-11 23:56:28 +0800

    :sparkle: Implemented onmessage api.

--- src/main/java/com/example/javaspringbootwebsocket/websocket/WebSocket.java
+++ src/main/java/com/example/javaspringbootwebsocket/websocket/WebSocket.java
@@ -23,6 +23,9 @@ public class WebSocket {
         this.userId = userId;
         webSocketMap.put(userId, this);
         log.info("【websocket消息】有新的连接，总数为:" + webSocketMap.size());
+        /*
+        * GroupSending is a method that sends a message to all users.
+        * */
         GroupSending("有新的连接，总数为:" + webSocketMap.size());
     }
 
@@ -31,6 +34,25 @@ public class WebSocket {
         webSocketMap.remove(this.userId);
         log.info("【websocket消息】连接断开，总数为:" + webSocketMap.size());
         GroupSending("连接断开，总数为:" + webSocketMap.size());
+    }
+
+    @OnMessage
+    public void onMessage(String messageStr) {
+        log.info("【websocket消息】收到客户端发来的消息:" + messageStr);
+
+        if (messageStr.indexOf("TOUSER") == 0) {
+            String[] split = messageStr.split(";");
+            Stringp[] split1 = split[0].split(":");
+            String[] split2 = split[1].split(":");
+
+            String userId = split1[1];
+            String message = split2[1];
+
+            AppointSending(userId, message);
+        } else {
+            // 群发
+            GroupSending(messageStr);
+        }
     }
 
 }
