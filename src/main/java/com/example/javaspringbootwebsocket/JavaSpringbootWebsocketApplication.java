fa8ea924d39bbe7a130d457f0ce064bfa2ee541d
zhangchuan <chuan@aspecta.id>
2024-01-11 23:30:04 +0800

    init repo.

---
+++ src/main/java/com/example/javaspringbootwebsocket/JavaSpringbootWebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaspringbootwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringbootWebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringbootWebsocketApplication.class, args);
+    }
+
+}
